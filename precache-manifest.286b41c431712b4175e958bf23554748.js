27e92538702bf5cebb735adeb293350755c16eec
Hyojung Kim <darkkong88@gmail.com>
2021-06-01 19:18:00 +0900

    Updates

---
+++ precache-manifest.286b41c431712b4175e958bf23554748.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4aad1f12d767942bd40acbe34b5854d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c5ff8bc8a7824af5f37",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "45f6ddef15f9cbb32d71",
+    "url": "./static/css/main.9f2aa95c.chunk.css"
+  },
+  {
+    "revision": "4c5ff8bc8a7824af5f37",
+    "url": "./static/js/2.c439dabd.chunk.js"
+  },
+  {
+    "revision": "8d4ef40ce22a6f0bff99c8edf92944f1",
+    "url": "./static/js/2.c439dabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f6ddef15f9cbb32d71",
+    "url": "./static/js/main.8367929e.chunk.js"
+  },
+  {
+    "revision": "6ddc2d6991b249ef72f7",
+    "url": "./static/js/runtime-main.7c4597cd.js"
+  },
+  {
+    "revision": "54232a2054f6c3cbfb57def936225fae",
+    "url": "./static/media/celebrity.54232a20.png"
+  },
+  {
+    "revision": "d3c7f31483ddb17be8b294bf923452a3",
+    "url": "./static/media/music-note.d3c7f314.png"
+  }
+]);
